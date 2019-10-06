a1c3c15512e0c03b07efdcd7e8f0874f1b78b09d
Paul Maltby <paul.maltby3@gmail.com>
2019-10-06 21:48:31 +0000

    homepage feed has tails from those following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def show
      @user = User.find_by(username: params[:username])
      @tweets = @user.tweets
+     @tweetings = Tweet.where(user: current_user.all_following)
+
   end
   
 end
