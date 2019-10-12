ca921fb3dd20f70b0abf5e5c4a1a28e0c1c31cf0
Paul Maltby <paul.maltby3@gmail.com>
2019-10-12 14:25:05 +0000

    tidying up x3

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,13 +4,13 @@ class VotesController < ApplicationController
   def create
      tweet = Tweet.find(params[:id])
      tweet.liked_by current_user
-     redirect_to user_path(current_user.username), notice: "You have successfully liked a tweet"
+     redirect_to :back, notice: "You have successfully liked a tweet"
   end
   
   def destroy
       tweet = Tweet.find(params[:id])
       tweet.unliked_by current_user
-      redirect_to user_path(current_user.username), notice: "You have successfully unliked a tweet"
+      redirect_to :back , notice: "You have successfully unliked a tweet"
   end
 
 
