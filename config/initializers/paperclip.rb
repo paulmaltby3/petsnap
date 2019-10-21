aca7b6f71ead56623f371212f8a5c068304a3905
Paul Maltby <paul.maltby3@gmail.com>
2019-10-21 19:47:30 +0000

    removed bits from paperclip.rb in config initializers

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,3 @@
     Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
     Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
